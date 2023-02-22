cda0a30e5e20b426cbef224dff11c1bf279a14c3
Mayank Jha <mjha199402@gmail.com>
2023-02-22 13:52:05 +0530

    Changes Added 3

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let extendParaBtn = document.getElementById('extendParaBtn');
 let extendedPara = document.getElementById('extendedPara');
 
-extendParaBtn.addEventListener('click', ()=>{
+extendParaBtn.addEventListener('click', () => {
     extendParaBtn.style.display = 'none';
     extendedPara.style.display = 'inline';
 })
@@ -9,12 +9,12 @@ extendParaBtn.addEventListener('click', ()=>{
 let hamburgerBtn = document.getElementById('hamburgerBtn');
 let list_items = document.getElementsByClassName('list-items')[0];
 
-hamburgerBtn.addEventListener('click', ()=>{
-    if(list_items.style.display == 'block'){
+hamburgerBtn.addEventListener('click', () => {
+    if (list_items.style.display == 'block') {
         list_items.style.display = 'none';
         hamburgerBtn.style.backgroundColor = '#bbbbbb';
     }
-    else{
+    else {
         list_items.style.display = 'block';
         hamburgerBtn.style.backgroundColor = '#1a93d8';
     }
