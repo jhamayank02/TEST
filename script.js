c5c375b40a2ad96898eee2cad6a2d394883de75e
Mayank Jha <mjha199402@gmail.com>
2023-02-26 20:36:55 +0530

    Added header

--- script.js
+++ script.js
@@ -19,3 +19,33 @@ hamburgerBtn.addEventListener('click', () => {
         hamburgerBtn.style.backgroundColor = '#1a93d8';
     }
 })
+
+let languageDropdown = document.getElementById('languageDropdown');
+let languageDropdownBG = document.getElementsByClassName('languageDropdownBG')[0];
+let languageOptions = document.getElementsByClassName('languageOptions')[0];
+
+languageDropdown.addEventListener('click', () => {
+    if (languageOptions.style.display == 'block') {
+        languageDropdownBG.style.backgroundImage = 'url(./images/arrow_drop_down.png)';
+        languageOptions.style.display = 'none';
+    }
+    else {
+        languageDropdownBG.style.backgroundImage = 'url(./images/arrow_drop_up.png)';
+        languageOptions.style.display = 'block';
+    }
+})
+
+let languageDropdownResponsive = document.getElementById('languageDropdownResponsive');
+let languageBox = document.getElementsByClassName('languageBox')[0];
+let languageDropdownBG_1 = document.getElementsByClassName('languageDropdownBG')[1];
+
+languageDropdownResponsive.addEventListener('click', () => {
+    if (languageBox.style.display == 'block') {
+        languageDropdownBG_1.style.backgroundImage = 'url(./images/arrow_drop_down.png)';
+        languageBox.style.display = 'none';
+    }
+    else {
+        languageDropdownBG_1.style.backgroundImage = 'url(./images/arrow_drop_up.png)';
+        languageBox.style.display = 'block';
+    }
+})
